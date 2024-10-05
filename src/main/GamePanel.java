d67c47e021949370ce81a7181c189892170faaea
ArtemZay <st8544346@stud.duikt.edu.ua>
2024-10-05 21:30:54 +0300

    commit

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final  int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -19,6 +21,7 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
 
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -38,7 +41,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
     @Override
     public void run() {
-        double drawInterval = (double) 1000000000 /FPS;
+        double drawInterval = 1000000000/FPS;
         double delta = 0;
         long lastTIme = System.nanoTime();
         long currentTime;
@@ -66,18 +69,7 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
     public void update(){
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed){
-            playerY += playerSpeed;
-        }
-        else if (keyH.leftPressed){
-            playerX -= playerSpeed;
-        }
-        else if (keyH.rightPressed){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     @Override
@@ -86,9 +78,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.WHITE);
-
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
